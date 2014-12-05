cc2967c64fc18fd130b5a2de62251c101c598167
yuany12 <thuyuany12@163.com>
2014-12-05 15:52:45 +0800

    init

---
+++ memoryMapping.v
@@ -0,0 +1,38 @@
+module memoryMapping (
+  input [15:0] virtualAddr,
+  output reg [15:0] actualRamAddr,
+  input [15:0] ramData,
+  input [7:0] serialPortData,
+  input [1:0] serialPortState,
+  output reg [15:0] realData,
+  output reg [1:0] index
+);
+
+localparam RAM = 2'b00,
+  SERIALPORT_DATA = 2'b10,
+  SERIALPORT_STATE = 2'b11;
+
+always @ (virtualAddr)
+begin
+  actualRamAddr = virtualAddr;
+  if (virtualAddr == 16'hbf00)
+    index = SERIALPORT_DATA;
+  else if (virtualAddr == 16'hbf01)
+    index = SERIALPORT_STATE;
+  else
+    index = RAM;
+end
+
+always @ (*)
+  case (index)
+    RAM:
+      realData = ramData;
+    SERIALPORT_DATA:
+      realData = {8'h00, serialPortData};
+    SERIALPORT_STATE:
+      realData = {14'b00000000000000, serialPortState};
+    default:
+      realData = 0;
+  endcase
+
+endmodule
---
+++ motherBoard.v
@@ -0,0 +1,116 @@
+module motherBoard (
+  input clk, rst, clkHand, clkUART,
+  inout [15:0] memDataBus,
+  output [17:0] memAddrBus,
+  output memRead, memWrite, memEnable,
+  output vgaHs, vgaVs,
+  output [2:0] vgaR, vgaG, vgaB,
+  output [15:0] leddebug,
+  input tbre, tsre, dataReady,    // wires linked with CPLD
+  inout [7:0] ram1DataBus,       // bus
+  output rdn, wrn,
+  output ram1Oe, ram1We, ram1En
+);
+
+wire [175:0] registerValue;
+wire [15:0] memAaddr, memBaddr, memAdataRead, memBdataRead, MeMemResult;
+wire [7:0] serialPortDataRead;
+wire [3:0] serialPortState;
+wire [1:0] memRW;
+wire [15:0] physicalMemAaddr, physicalMemBaddr;
+wire [15:0] ramAdataRead, ramBdataRead;
+wire [15:0] IfPC, IfIR;
+wire [15:0] ExCalResult, MeCalResult;
+wire [1:0] index;
+
+wire [3:0] registerS, registerM, IdRegisterT, MeRegisterT;
+
+reg clk25M, clk12M, clk6M;
+
+always @ (negedge clk, negedge rst)
+begin
+  if (!rst)
+    clk25M = 0;
+  else
+    clk25M = ~ clk25M;
+end
+
+always @ (negedge clk25M or negedge rst)
+  if (!rst)
+    clk12M = 0;
+  else
+    clk12M = ~ clk12M;
+	 
+always @ (negedge clk12M or negedge rst)
+  if (!rst)
+    clk6M = 0;
+  else
+    clk6M = ~ clk6M;
+
+assign leddebug = IfPC;//{MeMemResult, tbre, tsre, wrn, rdn, serialPortState};
+
+cpu naive (
+  clkUART, rst,
+  memAaddr, memBaddr,
+  ExCalResult, MeMemResult, memRW,
+  memAdataRead, memBdataRead,
+  registerValue,
+  IfPC, IfIR,
+  registerS, registerM, IdRegisterT, MeRegisterT,
+  MeCalResult
+);
+
+GraphicCard graphic (
+  clk25M, rst,
+  registerValue,
+  IfPC, IfIR,
+  registerS, registerM, IdRegisterT, MeRegisterT,
+  ExCalResult, MeCalResult,
+  vgaHs, vgaVs,
+  vgaR, vgaG, vgaB
+);
+
+memoryMapping mapingA (
+  memAaddr,
+  physicalMemAaddr,
+  ramAdataRead,
+  serialPortDataRead,
+  serialPortState[1:0],
+  memAdataRead,
+  index
+);
+
+memoryMapping mapingB (
+  memBaddr,
+  physicalMemBaddr,
+  ramBdataRead,
+  serialPortDataRead,
+  serialPortState[1:0],
+  memBdataRead
+);
+
+memoryController memory(
+  clkUART,
+  physicalMemAaddr, MeMemResult,
+  memRW,
+  ramAdataRead,
+  physicalMemBaddr,
+  ramBdataRead,
+  memDataBus,
+  memAddrBus,
+  memRead, memWrite, memEnable
+);
+
+serial_port uart(
+  clkUART, rst,
+  tbre, tsre, dataReady,
+  memRW, index,
+  MeMemResult,
+  ram1DataBus,
+  rdn, wrn,
+  ram1Oe, ram1We, ram1En,
+  serialPortDataRead,
+  serialPortState
+);
+
+endmodule
